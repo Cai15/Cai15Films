ad8d1b47bd53acb40f89baa653d1f0e2562fb14c
Cai15 <134505105+Cai15@users.noreply.github.com>
2023-09-25 21:47:20 +0300

    ViewBinding проба

--- app/src/main/java/com/example/cai15films/MainActivity.kt
+++ app/src/main/java/com/example/cai15films/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.cai15films
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cai15films.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding  //defining the binding class
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //initializing the binding class
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        binding.button2.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.button3.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.button4.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.button5.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
-    //Задание 19.9.1 (HW-03)
+
 }
