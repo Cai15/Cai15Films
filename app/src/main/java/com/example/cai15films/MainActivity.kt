2a4dde0fee744b855b551885d4952241d5b260a7
Cai15 <134505105+Cai15@users.noreply.github.com>
2023-09-28 23:51:28 +0300

    Переделка меню,снова доделать тосты

--- app/src/main/java/com/example/cai15films/MainActivity.kt
+++ app/src/main/java/com/example/cai15films/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.cai15films
 
 
-import android.content.Context
+
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cai15films.databinding.ActivityMainBinding
@@ -14,12 +13,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding  //defining the binding class
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) //initializing the binding class
         setContentView(binding.root)
 
-        binding.button1.setOnClickListener {
+        /*  binding.button1.setOnClickListener {
             Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
         }
         binding.button2.setOnClickListener {
@@ -36,6 +35,39 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }*/
+
+
+
+    }*/
+
+
+    topAppBar.setOnMenuItemClickListener {
+        when (it.itemId) {
+            R.id.settings -> {
+                Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
+        }
     }
 
+    bottom_navigation.setOnNavigationItemSelectedListener {
+
+        when (it.itemId) {
+            R.id.favorites -> {
+                Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.watch_later -> {
+                Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.selections -> {
+                Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
+        }
+    }
 }
